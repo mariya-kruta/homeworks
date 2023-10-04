2967312cfffb8a6355dbef0638d58cdf4878536a
mariya-kruta <omegalermontova@gmail.com>
2023-10-04 22:28:36 +0300

    delete '!' symbol

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        int age = 23;
 
-        String congratulation = "Happy 23 Birthday";
+        String congratulation = "Happy "+age+" Birthday";
         System.out.println(congratulation);
     }
 
