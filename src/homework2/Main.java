e922facb66f2e5f2e71929c84b20389bd65e7321
mariya-kruta <omegalermontova@gmail.com>
2023-12-06 19:52:59 +0200

    homework 2, part with Exc without searchSubject/searchGenre

---
+++ src/homework2/Main.java
@@ -0,0 +1,25 @@
+package homework2;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Library library1 = new Library();
+
+        library1.addBook(1000, new Book("1984", "J.Oruel", 1954));
+        library1.addBook(1001, new FictionBook("The last leaf", "O.Henry", 1914, "novel"));
+        library1.addBook(999, new NonFictionBook("Clean code", "I.Husky", 2018, "IT"));
+        library1.addBook(1003, new Book("Perfumer", "P.Suskind", 1741));
+        library1.addBook(1004, new NonFictionBook("TDCom", "R.Savin", 2003, "IT"));
+        library1.addBook(1005, new FictionBook("POCROV", "L.Dashvar", 2018, "modern UA"));
+        library1.addBook(1006, new Book("Hello", "Kroshka", 1954));
+        library1.deleteBook(1006);
+
+        library1.searchId(1001);
+        library1.searchTitle("TDCom");
+        library1.searchAuthor("Kroshka");
+        library1.searchYear(2018);
+        //library1.searchSubject("IT");
+    }
+}
